123ffdbe6db552dc6fcffde6d6a397c19d634f03
OvanBiervliet <ovan@coreland.be>
2023-02-06 14:34:43 +0100

    Swapped Return & Backspace

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -27,13 +27,13 @@ MIRYOKU_X(MOUSE,  "Mouse")
 &kp Q,             &kp W,             &kp F,             &kp P,             &kp G,             &kp J,             &kp L,             &kp U,             &kp Y,             &kp SQT,           \
 U_MT(LSHFT, A),    U_MT(LCTRL, R),    U_MT(LALT, S),     U_MT(LGUI, T),     &kp D,             &kp H,             U_MT(LGUI, N),     U_MT(LALT, E),     U_MT(LCTRL, I),    U_MT(LSHFT, O),    \
 &kp Z,             &kp X,             &kp C,             &kp V,             &kp B,             &kp K,             &kp M,             &kp COMMA,         &kp DOT,           &kp SLASH,         \
-U_NP,              U_NP,              U_LT(U_MOUSE, ESC),U_LT(U_NAV, SPC),  U_LT(U_NUM, TAB),  U_LT(U_FUN , RET), U_LT(U_SYM, BSPC), &kp DEL,           U_NP,              U_NP
+U_NP,              U_NP,              U_LT(U_MOUSE, ESC),U_LT(U_NAV, SPC),  U_LT(U_NUM, TAB),  U_LT(U_FUN , BSPC), U_LT(U_SYM, RET), &kp DEL,           U_NP,              U_NP
 
 #define MIRYOKU_LAYER_NAV \
 U_BOOT,            U_NA,              U_NA,              U_NA,              U_NA,              U_NA,              &kp LPAR,          &kp RPAR,          &kp MINUS,         &kp EQL,           \
 &kp LSHFT,         &kp LCTRL,         &kp LALT,          &kp LGUI,          U_NA,              &u_caps_word,      &kp LEFT,          &kp DOWN,          &kp UP,            &kp RIGHT,         \
 U_NA,              U_NA,              U_NA,              U_NA,              U_NA,              &kp INS,           &kp HOME,          &kp PG_DN,         &kp PG_UP,         &kp END,           \
-U_NP,              U_NP,              U_NA,              U_NA,              U_NA,              &kp RET,           &kp BSPC,          &kp DEL,           U_NP,              U_NP
+U_NP,              U_NP,              U_NA,              U_NA,              U_NA,              &kp BSPC,          &kp RET,          &kp DEL,           U_NP,              U_NP
 
 #define MIRYOKU_LAYER_SYM \
 &kp EXCL,          &kp AT,            &kp HASH,          &kp DLLR,          &kp PRCT,          &kp CRRT,          &kp AMPS,          &kp ASTRK,         &kp MINUS,         &kp EQL,           \
