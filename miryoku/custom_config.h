51ddc22b545baba2e7cdf4c2010896a00e6236ad
Olivier Van Biervliet <ovan@coreland.be>
2023-03-12 11:53:22 +0100

    Ultimate fix 2

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -29,12 +29,6 @@
         };
     };
 
-    #define U_IJ \
-    / { \
-            macros { \
-                ZMK_MACRO(u_ij, wait-ms = <40>; tap-ms = <40>; bindings = <&kp I &kp J>;) \
-            }; \
-        };
 
     //             ij: ij {
     //                 compatible = "zmk,behavior-macro";
@@ -63,6 +57,12 @@
     #define U_TAP    U_BASE
     #define U_BUTTON U_BASE
     #define U_MEDIA  U_BASE
+    #define U_IJ \
+    / { \
+        macros { \
+            ZMK_MACRO(u_ij, wait-ms = <40>; tap-ms = <40>; bindings = <&kp I &kp J>;) \
+        }; \
+    };
 
     #define MIRYOKU_LAYER_BASE \
     &kp Q,             &kp W,             &kp F,             &kp P,             &kp G,             &kp J,             &kp L,             &kp U,             &kp Y,             &kp SQT,           \
@@ -78,7 +78,7 @@
 
     #define MIRYOKU_LAYER_SYM \
     &kp EXCL,          &kp AT,            &kp HASH,          &kp DLLR,          &kp PRCT,          &kp CRRT,          &kp AMPS,          &kp ASTRK,         &kp MINUS,         &kp EQL,           \
-    &kp LPAR,          &kp LBKT,          &kp RBKT,          &kp RPAR,          &kp LS(LA(NUM_2)), &key_repeat,       &gmt LGUI MINUS,   &gmt LALT EQL,     &gmt LCTRL U_IJ,  &gmt LSHFT SEMI,    \
+    &kp LPAR,          &kp LBKT,          &kp RBKT,          &kp RPAR,          &kp LS(LA(NUM_2)), &key_repeat,       &gmt LGUI MINUS,   &gmt LALT EQL,     &gmt LCTRL U_IJ,   &gmt LSHFT SEMI,   \
     &kp LT,            &kp GRAVE,         &kp BSLH,          &kp GT,            &kp LA(NUM_3),     U_NA,              &kp C_PP,          &kp C_VOL_DN,      &kp C_VOL_UP,      &kp C_MUTE,        \
     U_NP,              U_NP,              &kp ESC,           &kp SPACE,         &kp TAB,           &bootloader,       U_NA,              &reset,            U_NP,              U_NP
 
